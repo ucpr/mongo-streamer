97896c4ea2223e409029b11f3798b7073d717725
ucpr <contact@ucpr.dev>
2023-11-21 00:43:09 +0900

    feat: impl graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"context"
 	"log/slog"
+	"os/signal"
+	"syscall"
+	"time"
 
 	_ "github.com/ucpr/mongo-streamer/internal/log"
 )
 
+const (
+	gracefulShutdownTimeout = 5 * time.Second
+)
+
 func main() {
-	slog.Info("Hello World")
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
+	defer stop()
+
+	<-ctx.Done()
+	tctx, cancel := context.WithTimeout(context.Background(), gracefulShutdownTimeout)
+	defer cancel()
+	_ = tctx
+
+	slog.Info("successfully graceful shutdown")
 }
