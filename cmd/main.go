65029631e3559fb8858c0a85940ad23af9643b75
ucpr <contact@ucpr.dev>
2023-11-23 04:22:49 +0900

    feat: run change stream watcher after startup

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,53 @@ package main
 
 import (
 	"context"
+	"log/slog"
 	"os/signal"
 	"syscall"
 	"time"
 
+	"github.com/ucpr/mongo-streamer/internal/config"
 	"github.com/ucpr/mongo-streamer/internal/log"
+	"github.com/ucpr/mongo-streamer/internal/mongo"
 )
 
 const (
 	gracefulShutdownTimeout = 5 * time.Second
 )
 
+func eventHandler(ctx context.Context, event *mongo.StreamObject) error {
+	log.Info("event", slog.Any("event", event))
+	return nil
+}
+
 func main() {
+	log.Info("initializing mongo-streamer")
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
 	defer stop()
+
+	cfg, err := config.NewMongoDB(ctx)
+	if err != nil {
+		log.Panic("failed to get environment variables", err)
+	}
+
+	mcli, err := mongo.NewClient(ctx, cfg)
+	if err != nil {
+		log.Panic("failed to create mongo client", err)
+	}
+
+	cs, err := mongo.NewChangeStream(ctx, mcli, "test", "test", eventHandler)
+	if err != nil {
+		log.Panic("failed to create change stream", err)
+	}
+	go func() {
+		log.Info("starting change stream watcher")
+		cs.Run()
+	}()
+	defer func() {
+		if err := cs.Close(ctx); err != nil {
+			log.Error("failed to close change stream", err)
+		}
+	}()
 
 	<-ctx.Done()
 	tctx, cancel := context.WithTimeout(context.Background(), gracefulShutdownTimeout)
--- internal/mongo/change_stream.go
+++ internal/mongo/change_stream.go
@@ -3,7 +3,10 @@ package mongo
 import (
 	"context"
 	"log/slog"
+	"time"
 
+	"github.com/ucpr/mongo-streamer/internal/log"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -24,10 +27,26 @@ type (
 	ChangeStreamOption func(opts *ChangeStreamOptions)
 
 	// StreamObject is a struct that represents a change stream object.
-	SteramObject struct{}
+	StreamObject struct {
+		ID struct {
+			Data string `bson:"_data"`
+		} `bson:"_id"`
+		OperationType  string           `bson:"operationType"`
+		ClusterTime    time.Time        `bson:"clusterTime"`
+		CollectionUUID primitive.Binary `bson:"collectionUUID"`
+		WallTime       time.Time        `bson:"wallTime"`
+		FullDocument   []byte           `bson:"fullDocument"`
+		Namespace      struct {
+			DB   string `bson:"db"`
+			Coll string `bson:"coll"`
+		} `bson:"ns"`
+		DocumentKey struct {
+			ID primitive.ObjectID `bson:"_id"`
+		} `bson:"documentKey"`
+	}
 
 	// ChangeStreamHandler is a type of handler function that handles ChangeStream.
-	ChangeStreamHandler func(ctx context.Context) error
+	ChangeStreamHandler func(ctx context.Context, event *StreamObject) error
 )
 
 // WithResumeToken sets the resume token for ChangeStream.
@@ -45,7 +64,7 @@ func WithBatchSize(batchSize int32) ChangeStreamOption {
 }
 
 // NewChangeStream creates a new change stream instance.
-func NewChangeStream(ctx context.Context, cli *Client, db, col string, opts ...ChangeStreamOption) (*ChangeStream, error) {
+func NewChangeStream(ctx context.Context, cli *Client, db, col string, handler ChangeStreamHandler, opts ...ChangeStreamOption) (*ChangeStream, error) {
 	chopts := &options.ChangeStreamOptions{}
 	for _, opt := range opts {
 		opt(&ChangeStreamOptions{chopts})
@@ -56,15 +75,24 @@ func NewChangeStream(ctx context.Context, cli *Client, db, col string, opts ...C
 	if err != nil {
 		return nil, err
 	}
-	cs := &ChangeStream{cs: changeStream}
+	cs := &ChangeStream{
+		cs:      changeStream,
+		handler: handler,
+	}
 	return cs, nil
 }
 
 // Run starts watching change stream.
 func (c *ChangeStream) Run() {
 	for c.cs.Next(context.Background()) {
-		if err := c.handler(context.Background()); err != nil {
-			slog.Error("failed to handle change stream", slog.String("err", err.Error()))
+		var streamObject StreamObject
+		if err := c.cs.Decode(&streamObject); err != nil {
+			log.Error("failed to decode steream object", slog.String("err", err.Error()))
+			continue
+		}
+
+		if err := c.handler(context.Background(), &streamObject); err != nil {
+			log.Error("failed to handle change stream", slog.String("err", err.Error()))
 		}
 	}
 }
